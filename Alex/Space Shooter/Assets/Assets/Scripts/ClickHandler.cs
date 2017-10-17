2ba94b28d7c2201faca81cf49c798419269484b6
Alexander Shaw <alexandershaw28@gmail.com>
2017-10-16 19:29:35 -0700

    Prototype1 Oct16
    Everything Alex completed by October 16th

---
+++ Alex/Space Shooter/Assets/Assets/Scripts/ClickHandler.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ClickHandler : MonoBehaviour {
+
+    public GameObject Master;
+    public GameObject Spawn;
+    private SpawnerScript SpwnScript;
+    private MasterScript MstrScript;
+    public int LifeCount = 3;
+
+
+    public void ButtonHey()
+    {
+       string j = EventSystem.current.currentSelectedGameObject.GetComponentInChildren<Text>().text;
+
+        print(j);
+    }
+
+
+    public void Reset()
+    {
+        MstrScript = Master.GetComponent<MasterScript>();
+        MstrScript.NewMultiplyProblem();
+        MstrScript.RandomlySetAnswers();
+    }
+
+    public void ButtonAnswer()
+    {
+        MstrScript = Master.GetComponent<MasterScript>();
+        SpwnScript = Spawn.GetComponent<SpawnerScript>();
+
+        string theButtonValue = EventSystem.current.currentSelectedGameObject.GetComponentInChildren<Text>().text;
+
+        if (theButtonValue == MstrScript.c.ToString())
+        {
+            SpwnScript.PlayerFireFirstRow();
+            print("CORRECT!");
+        }
+        else
+        {
+            SpwnScript.EnemyFire();
+            LifeCount = LifeCount - 1;
+            print("You now have this many lives: " + LifeCount.ToString());
+            print("TRY AGAIN.");
+        }
+
+        Reset();
+    }
+
+    public void Update()
+    {
+        if(LifeCount == 0)
+        {
+            print("YOU LOSE");
+        }
+    }
+}
---
+++ Alex/Space Shooter/Assets/Assets/Scripts/MasterScript.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MasterScript : MonoBehaviour
+{
+    public GameObject Spawn;
+    public GameObject[] ButtonArray;
+    private SpawnerScript SpwnScript;
+    public Text Txt;
+
+
+    public int LifeCount = 3; //if lifecount = 0, player loses, Game Over.
+    public int a;
+    public int b;
+    public int c;
+
+   public void NewMultiplyProblem()
+    {
+        a = Random.Range(1, 13);
+        b = Random.Range(1, 13);
+        c = a * b;
+
+        Txt.GetComponent<Text>().text = a.ToString() + " X " + b.ToString() + " = ?" ;
+
+
+        
+    }
+
+    public int RandomNotC()
+    {
+        int x = Random.Range(1, 145);
+
+        if(x == c)
+        {
+            RandomNotC();
+        }
+        
+        return x;
+        
+    }
+
+    public void RandomlySetAnswers()
+    {
+        int r = Random.Range(0, 5);  //Randomly picks array element number between 0 - 4;
+
+        
+
+        for(int i = 0; i < 5 ; i++)
+        {
+            
+
+            if(i == r)
+            {
+                ButtonArray[r].GetComponentInChildren<Text>().text = c.ToString();   //Randomly selected button is set = C;
+            }
+            else
+            {
+                ButtonArray[i].GetComponentInChildren<Text>().text = RandomNotC().ToString(); //Set all other buttons to be random
+            }
+
+            
+        }
+
+    }
+
+    // if user taps correct response
+    public void Correct()
+    {
+        SpwnScript = Spawn.GetComponent<SpawnerScript>();
+        //UI says "Correct"
+        SpwnScript.PlayerFireFirstRow();//Player Fires on first row
+        //Generate new math problem
+        //Move Enemy ships?
+    }
+    
+
+    //if user taps incorrect response
+    public void Incorrect()
+    {
+        SpwnScript = Spawn.GetComponent<SpawnerScript>();
+
+        //Player doesn't fire
+        //UI says "Incorrect"
+        SpwnScript.EnemyFire();//Enemy Ships FIRE
+        //LifeCount - 1;  //Life count -1 and UI displays this change
+        //Generate new math problem
+    }
+    
+    
+
+    // Use this for initialization
+    void Start()
+    {
+        SpwnScript = Spawn.GetComponent<SpawnerScript>();
+
+        
+        SpwnScript.SpawnRowOne(); // Spawn Row 1
+        SpwnScript.SpawnRowTwo();
+        SpwnScript.SpawnRowThree();
+        SpwnScript.SpawnRowFour();
+
+        NewMultiplyProblem();
+
+        RandomlySetAnswers();
+      
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
