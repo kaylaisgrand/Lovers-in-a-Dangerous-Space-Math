d5e32f9d716fa5f3d02e7b6a073b6ae7ceba6525
Alexander Shaw <alexandershaw28@gmail.com>
2017-11-24 16:39:38 -0800

    Level Select Added

---
+++ Alex/Space Shooter/Assets/Assets/Scripts/LevelSelector.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelector : MonoBehaviour {
+
+	public void LoadLevel1(){
+		SceneManager.LoadScene ("Alex Level 1");
+	}
+	public void LoadLevel2(){
+		SceneManager.LoadScene ("Alex Level 2");
+	}
+	public void LoadLevel3(){
+		SceneManager.LoadScene ("Alex Level 3");
+	}
+	public void LoadLevel4(){
+		SceneManager.LoadScene ("Alex Level 4");
+	}
+	public void LoadLevel5(){
+		SceneManager.LoadScene ("Alex Level 5");
+	}
+	public void LoadLevel6(){
+		SceneManager.LoadScene ("Alex Level 6");
+	}
+	public void LoadLevel7(){
+		SceneManager.LoadScene ("Alex Level 7");
+	}
+	public void LoadLevel8(){
+		SceneManager.LoadScene ("Alex Level 8");
+	}
+	public void LoadLevel9(){
+		SceneManager.LoadScene ("Alex Level 9");
+	}
+	public void LoadLevel10(){
+		SceneManager.LoadScene ("Alex Level 10");
+	}
+	public void LoadLevel11(){
+		SceneManager.LoadScene ("Alex Level 11");
+	}
+	public void LoadLevel12(){
+		SceneManager.LoadScene ("Alex Level 12");
+	}
+}
