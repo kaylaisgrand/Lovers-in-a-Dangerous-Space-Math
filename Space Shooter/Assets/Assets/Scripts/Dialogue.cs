65e208da76e3dbe8fd95d427456daefc1f16007e
ubcocosc <ubcocosc@gmail.com>
2017-11-29 22:36:35 -0800

    changes

---
+++ Space Shooter/Assets/Assets/Scripts/Dialogue.cs
@@ -0,0 +1,175 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+
+public class Dialogue : MonoBehaviour
+{
+
+  [TextArea(3, 10)]
+  private Text textComponent;
+  [TextArea(3, 10)]
+  public string[] DialogueStrings;
+  public float SecondsBetweenChars = 0.15f;
+  public float CharacterRateMultiplier = 0.5f;
+
+  public KeyCode DialogueInput = KeyCode.Return;
+
+  private bool isStringBeingRevealed = false;
+  private bool isDialoguePlaying = false;
+  private bool isEndOfDialogue = false;
+
+  public GameObject ContinueIcon;
+  public GameObject StopIcon;
+
+
+
+  public Text TextComponent
+  {
+    get
+    {
+      return TextComponent1;
+    }
+
+    set
+    {
+      TextComponent1 = value;
+    }
+  }
+
+  public Text TextComponent1
+  {
+    get
+    {
+      return textComponent;
+    }
+
+    set
+    {
+      textComponent = value;
+    }
+  }
+
+
+  // Use this for initialization
+  void Start()
+  {
+    TextComponent1 = GetComponent<Text>();
+    TextComponent1.text = "";
+
+    HideIcons();
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (Input.GetKeyDown(KeyCode.Return))
+    {
+      if (!isDialoguePlaying)
+      {
+        isDialoguePlaying = true;
+        StartCoroutine(StartDialogue());
+      }
+
+    }
+  }
+
+  private IEnumerator StartDialogue()
+  {
+    int dialogueLength = DialogueStrings.Length;
+    int currentDialogueIndex = 0;
+
+    while (currentDialogueIndex < dialogueLength || !isStringBeingRevealed)
+    {
+      if (!isStringBeingRevealed)
+      {
+        isStringBeingRevealed = true;
+        StartCoroutine(DisplayString(DialogueStrings[currentDialogueIndex++]));
+
+        if (currentDialogueIndex >= dialogueLength)
+        {
+          isEndOfDialogue = true;
+        }
+      }
+      yield return 0;
+    }
+    while (true)
+    {
+      if (Input.GetKeyDown(DialogueInput))
+      {
+        break;
+      }
+      yield return 0;
+    }
+
+    HideIcons();
+    isEndOfDialogue = false;
+    isDialoguePlaying = false;
+  }
+
+  private IEnumerator DisplayString(string stringToDisplay)
+  {
+    int stringLength = stringToDisplay.Length;
+    int currentCharIndex = 0;
+
+    HideIcons();
+
+    TextComponent1.text = "";
+
+    while (currentCharIndex < stringLength)
+    {
+      TextComponent1.text += stringToDisplay[currentCharIndex];
+      currentCharIndex++;
+
+      if (currentCharIndex < stringLength)
+      {
+        if (Input.GetKey(DialogueInput))
+        {
+          yield return new WaitForSeconds(SecondsBetweenChars * CharacterRateMultiplier);
+        }
+        else
+        {
+          yield return new WaitForSeconds(SecondsBetweenChars);
+        }
+      }
+      else break;
+    }
+
+    ShowIcon();
+
+    while (true)
+    {
+      if (Input.GetKeyDown(DialogueInput))
+      {
+        break;
+      }
+      yield return 0;
+    }
+
+    HideIcons();
+
+    isStringBeingRevealed = false;
+    TextComponent1.text = "";
+  }
+
+  private void HideIcons()
+  {
+    ContinueIcon.SetActive(false);
+    StopIcon.SetActive(false);
+  }
+
+  private void ShowIcon()
+  {
+    if (isEndOfDialogue)
+    {
+      StopIcon.SetActive(true);
+      return;
+    }
+    ContinueIcon.SetActive(true);
+  }
+
+}
+
---
+++ Space Shooter/Assets/Assets/Scripts/EnemyMath.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMath : MonoBehaviour {
+
+
+
+    public static int num1 = Random.Range(1, 13); //first number between 1 and 12
+    public static int num2 = Random.Range(1, 13); //second number between 1 and 12
+    public static int result = num1 * num2;
+
+    
+
+// Use this for initialization
+    void Start () {
+		//create 5 enemies
+	}
+
+
+
+
+
+   /* public static int Result()
+    {
+        int result = num1 * num2;  // third number is the result of the multiplication. 
+
+
+        //The ship whose number contains "result" is the ship the player needs to explode
+        return result;
+    }
+    */
+
+
+
+
+
+    
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
