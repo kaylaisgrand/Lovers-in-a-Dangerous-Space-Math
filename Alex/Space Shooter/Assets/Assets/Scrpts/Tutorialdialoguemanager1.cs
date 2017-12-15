83dcba2f119e31995d955a4352f01f4b575858ae
ubcocosc <ubcocosc@gmail.com>
2017-12-14 19:03:10 -0800

    Working on tutorial

---
+++ Alex/Space Shooter/Assets/Assets/Scrpts/Tutorialdialoguemanager1.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Tutorialdialoguemanager : MonoBehaviour
+{
+
+  public Text nameText;
+  public Text dialogueText;
+
+  private Queue<string> sentences;
+  // Use this for initialization
+  void Start()
+  {
+    sentences = new Queue<string>();
+  }
+
+  // Update is called once per frame
+  void Update() { }
+
+  public void StartDialogue(Dialogue dialogue)
+  {
+    nameText.text = dialogue.name;
+    sentences.Clear();
+
+    foreach (string line in dialogue.sentences)
+    {
+      sentences.Enqueue(line);
+    }
+
+    DisplayNextSentence();
+  }
+  public void DisplayNextSentence()
+  {
+    if (sentences.Count == 0)
+    {
+      EndDialogue();
+      return;
+    }
+    string line = sentences.Dequeue();
+    StopAllCoroutines();
+    StartCoroutine(TypeSentence(line));
+  }
+
+  IEnumerator TypeSentence(string line)
+  {
+    dialogueText.text = "";
+    foreach (char letter in line.ToCharArray())
+    {
+      dialogueText.text += letter;
+      yield return null;
+    }
+  }
+  void EndDialogue()
+  {
+    Debug.Log("End of conversation");
+  }
+
+}
