ea2465535df73be7eab17b80ad347818966d6aeb
kaylaisgrand <kaylaraine@hotmail.com>
2017-12-18 03:10:34 -0800

    added Facebook invite script plus button

---
+++ Alex/Space Shooter/Assets/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Facebook.Unity;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    public Text FriendsText;
+
+    private void Awake()
+    {
+        if (!FB.IsInitialized)
+        {
+            FB.Init(() =>
+            {
+                if (FB.IsInitialized)
+                    FB.ActivateApp();
+                else
+                    Debug.LogError("Couldn't initialize");
+            },
+            isGameShown =>
+            {
+                if (!isGameShown)
+                    Time.timeScale = 0;
+                else
+                    Time.timeScale = 1;
+            });
+        }
+        else
+            FB.ActivateApp();
+    }
+
+  
+    //change this is the part we need link should be to our app store
+    public void FacebookInvite()
+    {
+        FB.Mobile.AppInvite(new System.Uri("https://play.google.com/store/apps/details?id=com.tappybyte.byteaway"));
+    }
+
+}
+
